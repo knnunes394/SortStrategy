f34d72e8a1e7bb952d6b49fd3d5c2422b9a42092
knnunes394 <128314782+knnunes394@users.noreply.github.com>
2025-07-07 12:34:10 -0400

    Update selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -1,7 +1,7 @@
 public class SelectionSort  implements sortingContext{
 
 	@override 
-	static void sort(int[] array){
+	void sort(int[] array){
 		int n = array.length;
 		
 		for(int i = 0; i < (n - 1);i++){
@@ -17,20 +17,5 @@ public class SelectionSort  implements sortingContext{
 		}
 		
 	}
-	
-	static void printArray(int arr[]){
-        int i;
-        for (i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-	public static void main(String args[]){
-        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-        int n = arr.length;
-        selectSort(arr);
-        System.out.println("Sorted array: ");
-        printArray(arr);
-    }
 	
 }

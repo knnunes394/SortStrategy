577a17a6f2800b97cf327e4efc2418e682cc3d6a
knnunes394 <128314782+knnunes394@users.noreply.github.com>
2025-07-07 12:36:20 -0400

    Update client.java

--- client.java
+++ client.java
@@ -2,7 +2,7 @@ public class Client {
 
 
 	public static void main(String[] args){
-		SortingContext sortingContext = new SortingContext(new selectionSortStrategy());
+		SortingContext sortingContext = new SortingContext(new selectionSort());
 		int[] array1 = {5,2,9,1,5};
 		sortingContext.sort(array1);
 		

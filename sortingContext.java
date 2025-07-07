c6ed714efba91b02b9597e18284a46d0e3c0a5ba
knnunes394 <128314782+knnunes394@users.noreply.github.com>
2025-07-07 12:32:32 -0400

    Update sortingContext.java

--- sortingContext.java
+++ sortingContext.java
@@ -3,11 +3,11 @@ public class SortingContext {
 	private SortingContext sortingContext;
 	
 	public sortingContext(SortingContext sortingContext){
-		
+		this.sortingContext = sortingContext;
 	}
 	
 	public void setSortingContext(SortingContext sortingContext){
-	
+		this.sortingContext = sortingContext;
 	}
 	
 	public void performSorting(int[] array){

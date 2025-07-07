90f13a8f02066fc005ebeeb517663beadb4297e6
knnunes394 <128314782+knnunes394@users.noreply.github.com>
2025-07-06 20:06:43 -0400

    Create sortingContext.java

---
+++ sortingContext.java
@@ -0,0 +1,17 @@
+public class SortingContext {
+	
+	private SortingContext sortingContext;
+	
+	public sortingContext(SortingContext sortingContext){
+		
+	}
+	
+	public void setSortingContext(SortingContext sortingContext){
+	
+	}
+	
+	public void performSorting(int[] array){
+		sortingContext.sort(array);
+	}
+
+}
